f9a7e4e471807578ae23f9c87d4ed129c71adbe0
Quentin Hermiteau <qhermiteau@reply.com>
2026-03-17 12:22:14 +0100

    correction exo 8

--- src/app/exercices/8/page.jsx
+++ src/app/exercices/8/page.jsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useState } from "react";
+
 export default function FormBuilder() {
-  const formFields = [];
+  const [formFields, setFormFields] = useState([]);
 
   const handleAddFormField = (e) => {
+    e.preventDefault();
     const formData = new FormData(e.target);
 
     const newField = {
@@ -14,11 +17,25 @@ export default function FormBuilder() {
       required: formData.get("required"),
       value: "",
     };
+
+    setFormFields([...formFields, newField]);
   };
 
-  const handleUpdateFormField = (id, updatedField) => {};
+  const handleUpdateFormField = (id, { value }) => {
+    setFormFields(
+      formFields.map((field) => {
+        if (field.id !== id) {
+          return field;
+        }
 
-  const handleDeleteFormField = (id) => {};
+        return { ...field, value };
+      }),
+    );
+  };
+
+  const handleDeleteFormField = (id) => {
+    setFormFields(formFields.filter((field) => field.id !== id));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
