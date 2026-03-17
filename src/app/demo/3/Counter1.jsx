2a4118f1e45aeb60eda3f76f8a9f2e01fee8e5d2
Quentin Hermiteau <qhermiteau@reply.com>
2026-03-17 15:02:03 +0100

    fix demo 3

--- src/app/demo/3/Counter1.jsx
+++ src/app/demo/3/Counter1.jsx
@@ -1,23 +1,6 @@
 "use client";
 
-import { useReducer, useState } from "react";
-
-export default function CounterState() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => setCount(count + 1);
-  const handleDecrement = () => setCount(count - 1);
-
-  return (
-    <main>
-      <h1>{count}</h1>
-      <div>
-        <button onClick={handleDecrement}>-</button>
-        <button onClick={handleIncrement}>+</button>
-      </div>
-    </main>
-  );
-}
+import { useReducer } from "react";
 
 function reducer(state, value) {
   return state + value;
